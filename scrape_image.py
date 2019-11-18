94ccabb3a248099763419a9bf67c56c9a353f637
Tanish Grover <tanigrover@gmail.com>
2019-11-18 20:42:58 +0530

    add scrape image of the day from befind the search bar

---
+++ scrape_image.py
@@ -0,0 +1,22 @@
+import click
+import requests
+from bs4 import BeautifulSoup
+
+@click.command()
+def main():
+    url = 'https://unsplash.com/'
+    try:
+        html = requests.get(url).content
+        soup = BeautifulSoup(html, 'html.parser')
+        img = soup.find('picture').find('img')
+        img_url = img['src']
+        img_data = requests.get(img_url).content
+        with open('image_of_the_day.jpg', 'wb') as f:
+            f.write(img_data)
+        click.secho('Successfully Downloaded Image', fg='green')
+
+    except:
+        click.secho("Could not get image", fg='red')
+
+if __name__ == "__main__":
+    main()
